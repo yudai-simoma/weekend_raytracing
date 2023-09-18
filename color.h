f19988684ec412c08dbbcb7baf8adb9c9130b84a
yuda-simoma <cdcdasdll@icloud.com>
2023-09-18 17:48:09 +0900

    第1章、アンチエイリアシング

--- color.h
+++ color.h
@@ -5,11 +5,27 @@
 
 #include <iostream>
 
-void write_color(std::ostream &out, color pixel_color) {
+inline double clamp(double x, double min, double max) {
+  if (x < min) return min;
+  if (x > max) return max;
+  return x;
+}
+
+void write_color(std::ostream &out, color pixel_color, int samples_per_pixel) {
+  auto r = pixel_color.x();
+  auto g = pixel_color.y();
+  auto b = pixel_color.z();
+
+  // 色の和をサンプル数で割る
+  auto scale = 1.0 / samples_per_pixel;
+  r *= scale;
+  g *= scale;
+  b *= scale;
+
   // 各成分を [0,255] に変換して出力する
-  out << static_cast<int>(255.999 * pixel_color.x()) << ' '
-      << static_cast<int>(255.999 * pixel_color.y()) << ' '
-      << static_cast<int>(255.999 * pixel_color.z()) << '\n';
+  out << static_cast<int>(256 * clamp(r, 0.0, 0.999)) << ' '
+      << static_cast<int>(256 * clamp(g, 0.0, 0.999)) << ' '
+      << static_cast<int>(256 * clamp(b, 0.0, 0.999)) << '\n';
 }
 
 #endif
--- rtweekend.h
+++ rtweekend.h
@@ -5,7 +5,11 @@
 #include <cstdlib>
 #include <limits>
 #include <memory>
+#include <cstdlib>
 
+// 共通ヘッダー
+#include "ray.h"
+#include "vec3.h"
 
 // using
 using std::shared_ptr;
@@ -21,9 +25,23 @@ inline double degrees_to_radians(double degrees) {
   return degrees * pi / 180;
 }
 
-// 共通ヘッダー
-#include "ray.h"
-#include "vec3.h"
+inline double random_double() {
+  // [0,1) の実数乱数を返す
+  return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+  // [min,max) の実数乱数を返す
+  return min + (max-min)*random_double();
+}
+
+// C++の標準を使う場合は以下。
+// #include <random>
+// inline double random_double() {
+//   static std::uniform_real_distribution<double> distribution(0.0, 1.0);
+//   static std::mt19937 generator;
+//   return generator(distribution);
+// }
 
 #endif
 
